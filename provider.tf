f2efabf87ce78857746dac866ae33937eb88ee84
Fayas Akram <msfayas001@gmail.com>
2021-05-24 18:18:37 +0530

    BackTo Normal After Testing

--- provider.tf
+++ provider.tf
@@ -1,6 +1,20 @@
+terraform {
+  required_providers {
+    mongodbatlas = {
+      source  = "mongodb/mongodbatlas"
+      version = "0.9.1"
+    }
+  }
+}
+
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
   #profile    = "default"
-  region     = var.AWS_REGION
+  region = var.AWS_REGION
+}
+
+provider "mongodbatlas" {
+  public_key  = var.MONGODB_ATLAS_PUBLIC_KEY
+  private_key = var.MONGODB_ATLAS_PRIVATE_KEY
 }
--- vars.tf
+++ vars.tf
@@ -1,6 +1,13 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
+
 variable "AWS_REGION" {
   default   = "us-west-1"
   sensitive = true
+}
+variable "MONGODB_ATLAS_PUBLIC_KEY" {}
+variable "MONGODB_ATLAS_PRIVATE_KEY" {}
+
+variable "x" {
+  type =string
 }
