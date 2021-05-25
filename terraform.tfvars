c67b031a81e9739e5c7ffdb7756717cbac9e47d9
Fayas Akram <msfayas001@gmail.com>
2021-05-25 15:24:29 +0530

    vars commited

--- terraform.tfvars
+++ terraform.tfvars
@@ -5,3 +5,9 @@ AWS_REGION = "us-west-1"
 
 MONGODB_ATLAS_PUBLIC_KEY = "xxxx"
 MONGODB_ATLAS_PRIVATE_KEY = "xxxx"
+
+atlas_cidr = "192.168.248.0/21"
+atlas_org_id = "FAYAS'S ORG - 2020-07-23"
+
+atlas_username = "fayas_akram"
+atlas_password = "password1234"
--- vars.tf
+++ vars.tf
@@ -7,6 +7,24 @@ variable "AWS_REGION" {
 }
 variable "MONGODB_ATLAS_PUBLIC_KEY" {}
 variable "MONGODB_ATLAS_PRIVATE_KEY" {}
+variable "MONGODB_ATLAS" {
+  default   = "US-WEST-1"
+}
+
+variable "atlas_cidr" {
+  description = "Atlas VPC CIDR"
+}
+variable "atlas_org_id" {
+  description = "Atlas Orgainzation ID"
+}
+
+variable "altas_username" {
+  description = "Atlas User Name"
+}
+
+variable "atlas_password" {
+  description = "Atlas Password"
+}
 
 variable "x" {
   type =string
